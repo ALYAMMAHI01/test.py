290b7d32a9e246e5bb2cc03f367e05116e5b6c2f
ALYAMMAHI01 <bindawei01@gmail.com>
2025-12-15 09:13:01 +0400

    test
    my first code

---
+++ test m1.py
@@ -0,0 +1,7 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("hello, github")
+hello, github
+>>> print("I wrote my first python code")
+I wrote my first python code
+>>>
